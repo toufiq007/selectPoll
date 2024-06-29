92488d92166862e344dd43ac731ea15f781a10af
toufiq007 <t.limon2342@gmail.com>
2024-06-30 01:12:37 +0600

    add middlewares

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,11 +1,14 @@
+import express from "express";
+import mongoose from "mongoose";
+import morgran from "morgan";
 
-import express from "express"
-import mongoose from "mongoose"
-import morgran from "morgan"
+const app = express();
 
-const app = express()
+// middlewares
+app.use(morgran("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-
-app.listen(4000,()=>{
-    console.log('server is running')
-})
+app.listen(4000, () => {
+  console.log("server is running");
+});
